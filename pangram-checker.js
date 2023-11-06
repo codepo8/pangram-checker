1675c95a1d794f3fda7dc0e0f3efb68fc09dd6f7
Christian Heilmann <chris.heilmann@gmail.com>
2023-11-06 09:32:40 +0100

    cleanup

--- pangram-checker.js
+++ pangram-checker.js
@@ -1,14 +1,26 @@
+let pangrams= [
+    "The quick brown fox jumps over the lazy dog.",
+    "Sphinx of black quartz, judge my vow.",
+    "Pack my box with five dozen liquor jugs.",
+    "Jackdaws love my big sphinx of quartz.",
+    "The five boxing wizards jump quickly.",
+    "How vexingly quick daft zebras jump!",
+    "Bright vixens jump; dozy fowl quack.",
+    "The jay, pig, fox, zebra, and my wolves quack!",
+    "Sympathizing would fix Quaker objectives.",
+    "Fickle jinx bog dwarves spy math quiz.",
+    "Big dwarves heckle my top quiz of jinxed vows.",
+    "Go, lazy fat vixen; be shrewd, jump quick."
+];
 const f = document.querySelector('form');
 const sentencefield = document.querySelector('#sentence');
 const result = document.querySelector('.result');
 const demos = document.querySelector('#demos');
 
-const check = _ => {
+const check = sentence => {
     const sentence = sentencefield.value;
     const letters = 'abcdefghijklmnopqrstuvwxyz';
-    console.log(sentence);
     const chars = sentence.toLowerCase().replace(/[^a-z]/g, '').split(''); 
-    console.log(chars);
     const uniqueLetters = new Set(chars);
     if (uniqueLetters.size === 26) {
         let out = 'This is a pangram.';
@@ -37,30 +49,12 @@ const check = _ => {
     }
 }
 sentencefield.addEventListener ('keyup', (e) => {
-    check();
+    check(sentencefield.value);
 })
 f.addEventListener('submit', (e) => {
     e.preventDefault();
-    check();
+    check(sentencefield.value);
 })
-
-let pangrams= [
-    "The quick brown fox jumps over the lazy dog.",
-    "Sphinx of black quartz, judge my vow.",
-    "Pack my box with five dozen liquor jugs.",
-    "Jackdaws love my big sphinx of quartz.",
-    "The five boxing wizards jump quickly.",
-    "How vexingly quick daft zebras jump!",
-    "Bright vixens jump; dozy fowl quack.",
-    "The jay, pig, fox, zebra, and my wolves quack!",
-    "Sympathizing would fix Quaker objectives.",
-    // "The explorer was frozen in his big kayak just after making queer discoveries.",
-    // "Six big devils from Japan quickly forgot how to waltz.",
-    // "Big July earthquakes confound zany experimental vow.",
-    "Fickle jinx bog dwarves spy math quiz.",
-    "Big dwarves heckle my top quiz of jinxed vows.",
-    "Go, lazy fat vixen; be shrewd, jump quick."
-];
 pangrams.forEach(pangram => {
     const li = document.createElement('li');
     const button = document.createElement('button');
@@ -71,6 +65,6 @@ pangrams.forEach(pangram => {
 demos.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
         sentencefield.value = e.target.textContent;
-        check();
+        check(sentencefield.value);
     }
 })
